6505a15f460b3808a453181721cc4f6ceea68c3e
RAFAEL-NOTEBOOK\Rafael <rafaelsmag@gmail.com>
2017-10-10 19:46:33 -0300

    Passando valores pela propriedade do componente

--- ex.2/index.jsx
+++ ex.2/index.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom'
 
 import Component from './component'
 
-ReactDOM.render(<Component value="Hello!!" />, document.getElementById('app'))
+ReactDOM.render(<Component />, document.getElementById('app'))
