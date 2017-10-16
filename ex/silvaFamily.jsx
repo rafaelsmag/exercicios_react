d710893b0614dcc79c559fb3b2cc008c585d6a38
RAFAEL-NOTEBOOK\Rafael <rafaelsmag@gmail.com>
2017-10-16 20:09:16 -0200

    Componente dentor de componente

---
+++ ex/silvaFamily.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Member from './member'
+
+export default () => (
+  <div>
+    <Member name="Rafael" lastName="Silva" />
+    <Member name="João" lastName="Silva" />
+    <Member name="Marisa" lastName="Silva" />
+    <Member name="Ana" lastName="Silva" />
+  </div>
+)
